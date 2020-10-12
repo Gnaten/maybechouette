d6f075a6b0e060e00f1e444a9a6594681fc43905
Gnaten <nathan01010000@gmail.com>
2020-10-11 23:00:37 -0700

    Version 1.2 Alpha Male Remastered Unleashed Volume 1

--- beep_2.py
+++ beep_2.py
@@ -6,26 +6,8 @@ led = [7, 11, 12, 13, 15, 38, 16, 40, 18, 22]		#If using lists, make sure the nu
 
 def setup():
 	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(7, GPIO.OUT)		#Setting up pins only for providing power
-	GPIO.setup(11, GPIO.OUT)
-	GPIO.setup(12, GPIO.OUT)
-	GPIO.setup(13, GPIO.OUT)
-	GPIO.setup(15, GPIO.OUT)
-	GPIO.setup(38, GPIO.OUT)
-	GPIO.setup(16, GPIO.OUT)
-	GPIO.setup(40, GPIO.OUT)
-	GPIO.setup(18, GPIO.OUT)
-	GPIO.setup(22, GPIO.OUT)
-	GPIO.output(7, 0)			#Pins 7, 11, 12, and 13 are set to GPIO.LOW in output
-	GPIO.output(11, 0)
-	GPIO.output(12, 0)
-	GPIO.output(13, 0)
-	GPIO.output(15, 0)
-	GPIO.output(38, 0)
-	GPIO.output(16, 0)
-	GPIO.output(40, 0)
-	GPIO.output(18, 0)
-	GPIO.output(22, 0)
+	GPIO.setup(led, GPIO.OUT)		#Setting up pins only for providing power
+	GPIO.output(led, 0)			#Pins 7, 11, 12, and 13 are set to GPIO.LOW in output
 	print('Setup Complete')
 	time.sleep(1)
 
