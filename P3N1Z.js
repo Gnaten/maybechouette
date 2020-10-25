bcad7c003a19ef166f3d7080fb99f1ed7792c33e
Gnaten <nathan01010000@gmail.com>
2020-10-24 18:14:15 -0700

    Autobots Rollout

---
+++ P3N1Z.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Mein Skript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Disables Instagram Pop-up
+// @author       Gnate
+// @match        file:///C:/Users/Natha/Desktop/Code/websocket/Website2Hak/Website2Hak.html
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Your cum here...
+})();
